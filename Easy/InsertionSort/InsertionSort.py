792619a40330a551c7aec66efec4806fd852b6b2
Kevin Cruz <kocruz.24@gmail.com>
2021-08-11 20:25:26 -0400

    Add Insertion sort solution (8/11/21)

---
+++ Easy/InsertionSort/InsertionSort.py
@@ -0,0 +1,20 @@
+def swap(idx_one, idx_two, array):
+    temp = array[idx_one]
+    array[idx_one] = array[idx_two]
+    array[idx_two] = temp
+
+
+# O(N^2) time | O(1) space
+def insertion_sort(array):
+    for i in range(1, len(array)):
+        j = i
+        while j > 0 and array[j] < array[j - 1]:
+            swap(j, j - 1, array)
+            j -= 1
+    return array
+
+
+arr = [8, 5, 2, 9, 5, 6, 3]
+
+if __name__ == '__main__':
+    print(insertion_sort(arr))
