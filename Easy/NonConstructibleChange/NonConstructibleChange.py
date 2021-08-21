397c3ffa3cba6bb2803b86226af63f348d45b741
Kevin O. Cruz Ojeda <kocruz.24@gmail.com>
2021-08-21 02:32:08 -0400

    Add NonConstructibleChange solution - Not my own (8/20/21)
    Didn't do the pseudcode

---
+++ Easy/NonConstructibleChange/NonConstructibleChange.py
@@ -0,0 +1,12 @@
+# O(Nlog(N)) time | O(1) space
+def nonConstructibleChange(coins):
+	coins.sort()
+	change = 0
+	
+	for coin in coins:
+		if coin > change + 1:
+			return change + 1
+			
+		change += coin
+	
+	return change + 1
