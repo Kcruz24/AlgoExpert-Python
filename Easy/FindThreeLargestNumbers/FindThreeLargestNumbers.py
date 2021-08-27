269a462567ac4d3510f54189c03bbdb1c67ee297
Kevin O. Cruz Ojeda <32002210+Kcruz24@users.noreply.github.com>
2021-08-27 03:27:51 -0400

    Update FindThreeLargestNumbers.py

--- Easy/FindThreeLargestNumbers/FindThreeLargestNumbers.py
+++ Easy/FindThreeLargestNumbers/FindThreeLargestNumbers.py
@@ -1,7 +1,6 @@
 # O(N) time | O(1) space
 def findThreeLargestNumbers(array):
 	largestNums = [array[0], array[1], array[2]]
-	largestNums.sort()
 	
 	for idx, num in enumerate(array):
 		if num > largestNums[0] and num not in largestNums:
