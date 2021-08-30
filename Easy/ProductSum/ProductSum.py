f2872a25eb1e8f6f8147bed7ad8df0292b7c7034
Kevin Cruz <kocruz.24@gmail.com>
2021-08-29 23:02:13 -0400

    Update ProductSum.py

--- Easy/ProductSum/ProductSum.py
+++ Easy/ProductSum/ProductSum.py
@@ -1,15 +1,3 @@
-# Tip: You can use the type(element) function to check whether an item
-# is a list or an integer.
-def productSum2(array, depth=1):
-    sum = 0
-    for elem in array:
-        if type(elem) is list:
-            sum += productSum(elem, depth + 1)
-        else:
-            sum += elem
-    return sum * depth
-
-
 # O(N) time | O(D) space - where "D" is the innermost depth of the levels of
 # the lists
 def productSum(array, depth=1):
