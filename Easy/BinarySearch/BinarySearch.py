d76644913fefb411a98a8e4d0962bd8fccd7c226
Kevin Cruz <kocruz.24@gmail.com>
2021-08-29 23:02:37 -0400

    Add recursive approach to BinarySearch.py

--- Easy/BinarySearch/BinarySearch.py
+++ Easy/BinarySearch/BinarySearch.py
@@ -1,3 +1,4 @@
+# Iterative approach
 # O(log(N)) time | O(1) space
 def binarySearch(array, target):
     start = 0
@@ -13,6 +14,26 @@ def binarySearch(array, target):
             start = mid + 1
 
     return - 1
+
+
+# Recursive approach
+# O(log(N)) time | O(log(N)) space
+def binarySearchRecursive(array, target):
+    return binarySearchHelper(array, target, 0, len(array) - 1)
+
+
+def binarySearchHelper(array, target, start, end):
+    if start > end:
+        return -1
+
+    mid = (start + end) // 2
+
+    if target == array[mid]:
+        return mid
+    elif target < array[mid]:
+        return binarySearchHelper(array, target, start, mid - 1)
+    elif target > array[mid]:
+        return binarySearchHelper(array, target, mid + 1, end)
 
 
 arr = [0, 1, 21, 33, 45, 45, 61, 71, 72, 73]
