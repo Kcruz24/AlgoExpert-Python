60cd62b91d97a8728dff125ccc3a843cad975c1e
Kevin Cruz <kocruz.24@gmail.com>
2021-09-09 20:49:46 -0400

    Update ValidateSubsequence solution

--- Easy/ValidateSubsequence/ValidateSubsequence.py
+++ Easy/ValidateSubsequence/ValidateSubsequence.py
@@ -1,43 +1,20 @@
 # O(N) time | O(1) space
 def isValidSubsequence(array, sequence):
-    if len(sequence) > len(array) or sequence[0] not in array:
-        return False
+    seqIdx = 0
 
-    if all(elem == sequence[0] for elem in array):
-        return True
+    for num in array:
+        if seqIdx < len(sequence) and num == sequence[seqIdx]:
+            seqIdx += 1
 
-    current_idx = 0
-
-    for i in range(1, len(sequence)):
-        if sequence[i] in array:
-            array_idx = array.index(sequence[i])
-            if array_idx > current_idx:
-                current_idx = array_idx
-            else:
-                return False
-        else:
-            return False
-    return True
-
-
-# Algo Expert Solution
-# O(N) time | O(1) space
-def isValidSubsequence2(array, sequence):
-    arr_idx = 0
-    seq_idx = 0
-
-    while arr_idx < len(array) and seq_idx < len(sequence):
-        if sequence[seq_idx] == array[arr_idx]:
-            seq_idx += 1
-        arr_idx += 1
-
-    return seq_idx == len(sequence)
+    return seqIdx == len(sequence)
 
 
 arr = [5, 1, 22, 25, 6, -1, 8, 10]
-arr2 = [1, 1, 1, 1, 1]
 seq = [1, 6, -1, 10]
+
+arr2 = [1, 1, 1, 1, 1]
 seq2 = [1, 1, 1]
+
 arr3 = [5, 1, 22, 25, 6, -1, 8, 10]
 seq3 = [5, 1, 22, 22, 25, 6, -1, 8, 10]
 
@@ -45,4 +22,4 @@ arr4 = [5, 1, 22, 25, 6, -1, 8, 10]
 seq4 = [26]
 
 if __name__ == '__main__':
-    print(isValidSubsequence2(arr2, seq2))
+    print(isValidSubsequence(arr2, seq2))
