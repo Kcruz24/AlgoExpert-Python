76d8f0d10562b3c927574d8280e2e5c9bb4d043b
Kevin O. Cruz <kocruz.24@gmail.com>
2021-12-07 09:10:35 -0400

    Add iterative Depth-First Search

--- Easy/DepthFirstSearch/DepthFirstSearch.py
+++ Easy/DepthFirstSearch/DepthFirstSearch.py
@@ -13,13 +13,25 @@ class Node:
 		for child in self.children:
 			child.depthFirstSearch(array)
 		return array
+
+	# O(v + e) time | O(v) space
+	def depthFirstSearchIterative(self, array):
+		stack = [self]
+		while len(stack) != 0:
+			node = stack.pop()
+			array.append(node.name)
+			for i in range (len(node.children)-1, -1, -1):
+				stack.append(node.children[i])
+		return array
 		
+if __name__ == '__main__':
+	graph = Node("A")
+	graph.addChild("B").addChild("C").addChild("D")
+	graph.children[0].addChild("E").addChild("F")
+	graph.children[2].addChild("G").addChild("H")
+	graph.children[0].children[1].addChild("I").addChild("J")	
+	graph.children[2].children[0].addChild("K")
 
-graph = Node("A")
-graph.addChild("B").addChild("C").addChild("D")
-graph.children[0].addChild("E").addChild("F")
-graph.children[2].addChild("G").addChild("H")
-graph.children[0].children[1].addChild("I").addChild("J")	
-graph.children[2].children[0].addChild("K")
+	print(graph.depthFirstSearchIterative([]) == ["A", "B", "E", "F", "I", "J", "C", "D", "G", "K", "H"])
 
-print(graph.depthFirstSearch([]))
+	
