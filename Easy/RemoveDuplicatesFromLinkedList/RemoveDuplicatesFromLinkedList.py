cc32d3079b6c37d5f63d9a372d844db6c54c81e4
Kevin O. Cruz <kocruz.24@gmail.com>
2021-12-07 09:14:00 -0400

    Update RemoveDuplicatesFromLinkedList

--- Easy/RemoveDuplicatesFromLinkedList/RemoveDuplicatesFromLinkedList.py
+++ Easy/RemoveDuplicatesFromLinkedList/RemoveDuplicatesFromLinkedList.py
@@ -2,10 +2,10 @@
 def removeDuplicatesFromLinkedList(linkedList):
 	travNode = linkedList
 	
-	while(travNode is not None):
-		while(travNode.next is not None and travNode.value == travNode.next.value):
+	while travNode is not None:
+		while travNode.next is not None and travNode.value == travNode.next.value:
 			travNode.next = travNode.next.next
 		
 		travNode = travNode.next
-	
+
 	return linkedList
