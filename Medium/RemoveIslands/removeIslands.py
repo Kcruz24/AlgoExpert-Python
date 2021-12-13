06407832a860a8453c5833495bceaa3e3a9661ff
Kevin O. Cruz <kocruz.24@gmail.com>
2021-12-13 19:37:52 -0400

    Update removeIslands() code and pseudocode

--- Medium/RemoveIslands/removeIslands.py
+++ Medium/RemoveIslands/removeIslands.py
@@ -1,105 +1,78 @@
-# Algo Expert optimal solution
+
 # O(wh) time | O(wh) space
 def removeIslands(matrix):
-	for row in range(len(matrix)):
-		for col in range(len(matrix[row])):
-			row_is_border = row == 0 or row == len(matrix) - 1
-			col_is_border = col == 0 or col == len(matrix[row]) - 1
+    last_row = len(matrix) - 1
+    last_col = len(matrix[0]) - 1
 
-			is_border = row_is_border or col_is_border
+    for row in range(len(matrix)):
+        for col in range(len(matrix[row])):
+            row_is_border = row == 0 or row == last_row
+            col_is_border = col == 0 or col == last_col
 
-			if not is_border:
-				continue
+            is_border = row_is_border or col_is_border
 
-			if matrix[row][col] != 1:
-				continue
+            if not is_border or matrix[row][col] != 1:
+                continue
 
-			change_ones_connected_to_border_to_twos(matrix, row, col)
+            change_ones_to_twos(matrix, row, col)
 
-	for row in range(len(matrix)):
-		for col in range(len(matrix[row])):
+    for row in range(len(matrix)):
+        for col in range(len(matrix[row])):
+            if matrix[row][col] > 0:
+                matrix[row][col] -= 1
+            
+    return matrix
 
-			color = matrix[row][col]
-			if color == 1:
-				matrix[row][col] = 0
-			elif color == 2:
-				matrix[row][col] = 1
+def change_ones_to_twos(matrix, row, col):
+    stack = [(row, col)]
 
-	return matrix
+    while len(stack) > 0:
+        current_row, current_col = stack.pop()
+        
+        neighbors = get_neigbhors(matrix, current_row, current_col)
+        for row, col in neighbors:
+            if matrix[row][col] == 1:
+                stack.append((row, col))
+            
+        matrix[current_row][current_col] = 2
 
-# Depth-first search
-def change_ones_connected_to_border_to_twos(matrix, start_row, start_col):
-	stack = [(start_row, start_col)]
-
-	while len(stack) > 0:
-		current_position = stack.pop()
-		current_row, current_col = current_position
-
-		matrix[current_row][current_col] = 2
-
-		neighbors = get_neigbhors(matrix, current_row, current_col)
-		for neighbor in neighbors:
-			row, col = neighbor
-
-			if matrix[row][col] != 1:
-				continue
-
-			stack.append(neighbor)
-
+        
+        
 def get_neigbhors(matrix, row, col):
-	neighbors = []
+    neighbors = []
 
-	num_rows = len(matrix)
-	num_cols = len(matrix[row])
+    row_length = len(matrix)
+    col_length = len(matrix[row])
 
-	if row - 1 >= 0: #UP
-		neighbors.append((row - 1, col))
-	if row + 1 < num_rows: # DOWN
-		neighbors.append((row + 1, col))
-	if col - 1 >= 0: # LEFT
-		neighbors.append((row, col - 1))
-	if col + 1 < num_cols:
-		neighbors.append((row, col + 1))
 
-	return neighbors
+    if row - 1 >= 0: # UP
+        neighbors.append((row - 1, col))
+    if row + 1 < row_length:
+        neighbors.append((row + 1, col))
+    if col - 1 >= 0:
+        neighbors.append((row, col - 1))
+    if col + 1 < col_length:
+        neighbors.append((row, col + 1))
+
+    return neighbors
 
 
 if __name__ == '__main__':
-	sample_input = [
-            [1, 0, 0, 0, 0, 0],
-            [0, 1, 0, 1, 1, 1],
-            [0, 0, 1, 0, 1, 0],
-            [1, 1, 0, 0, 1, 0],
-            [1, 0, 1, 1, 0, 0],
-            [1, 0, 0, 0, 0, 1],
-        ]
-
-	expected = [
-            [1, 0, 0, 0, 0, 0],
-            [0, 0, 0, 1, 1, 1],
-            [0, 0, 0, 0, 1, 0],
-            [1, 1, 0, 0, 1, 0],
-            [1, 0, 0, 0, 0, 0],
-            [1, 0, 0, 0, 0, 1],
-        ]
-
-
-	actual = removeIslands(sample_input)
-	print(actual == expected)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    sample_input = [
+        [1, 0, 0, 0, 0, 0],
+        [0, 1, 0, 1, 1, 1],
+        [0, 0, 1, 0, 1, 0],
+        [1, 1, 0, 0, 1, 0],
+        [1, 0, 1, 1, 0, 0],
+        [1, 0, 0, 0, 0, 1],
+    ]
+    expected = [
+        [1, 0, 0, 0, 0, 0],
+        [0, 0, 0, 1, 1, 1],
+        [0, 0, 0, 0, 1, 0],
+        [1, 1, 0, 0, 1, 0],
+        [1, 0, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0, 1],
+    ]
+    actual = removeIslands(sample_input)
+    print(actual == expected)
