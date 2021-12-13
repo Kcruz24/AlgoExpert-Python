371d90e1baf046c14ef095c8a4905d557565fe43
Kevin O. Cruz <kocruz.24@gmail.com>
2021-12-13 19:37:17 -0400

    Update kadanesAlgorithm pseudocode

--- Medium/KadanesAlgorithm/kadanesAlgorithm.py
+++ Medium/KadanesAlgorithm/kadanesAlgorithm.py
@@ -37,5 +37,5 @@ if __name__ == '__main__':
     array = [3, 5, -9, 1, 3, -2, 3, 4, 7, 2, -9, 6, 3, 1, -5, 4]
     array2 = [-1, -2, -3, -4, -5, -6, -7, -8, -9, -10]
 
-    print(kadanesAlgorithm2(array))
+    print(kadanesAlgorithm(array))
     print(kadanesAlgorithm2(array2))
