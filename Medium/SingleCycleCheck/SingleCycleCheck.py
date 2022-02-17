1fbe4f92bc914c39eb060184b0a11010e9779cbd
Kevin O. Cruz <kocruz.24@gmail.com>
2022-02-16 23:58:12 -0400

    Add Single Cycle Check - My Solution

--- Medium/SingleCycleCheck/SingleCycleCheck.py
+++ Medium/SingleCycleCheck/SingleCycleCheck.py
@@ -1,3 +1,4 @@
+# AlgoExpert Solution
 # O(N) time | O(1) space
 def hasSingleCycle(array):
 	num_elements_visited = 0
@@ -12,6 +13,26 @@ def hasSingleCycle(array):
 	return cur_idx == 0
 
 
+# My Solution
+# O(N) time | O(N) space
+def hasSingleCycle2(array):
+    curr_idx = getNextIdx(0, array)
+    visited_idx = []
+
+    for _ in range(len(array)):
+        if curr_idx not in visited_idx:
+            visited_idx.append(curr_idx)
+            curr_idx = getNextIdx(curr_idx, array)
+
+            if curr_idx in visited_idx:
+                return False
+
+            if curr_idx not in visited_idx and len(visited_idx) == len(array) - 1:
+                return True
+
+    return False
+
+
 def getNextIdx(cur_idx, array):
 	return (array[cur_idx] + cur_idx) % len(array)
 
@@ -19,4 +40,4 @@ def getNextIdx(cur_idx, array):
 if __name__ == '__main__':
 	array = [2, 3, 1, -4, -4, 2]
 
-	print(hasSingleCycle(array))
+	print(hasSingleCycle2(array))
